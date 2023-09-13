319d2b5419a932e35bc4531ce4194916d169d945
BolitaDePelos <78169105+BolitaDePelos@users.noreply.github.com>
2023-09-13 18:28:43 -0500

    Knife step start

--- Arunuka lab/Assets/Scripts/Tutorial/CuttingManager.cs
+++ Arunuka lab/Assets/Scripts/Tutorial/CuttingManager.cs
@@ -1,5 +1,7 @@
+using Newtonsoft.Json.Bson;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using UnityEngine;
 
 public class CuttingManager : MonoBehaviour
@@ -8,11 +10,15 @@ public class CuttingManager : MonoBehaviour
     [SerializeField]
     private List<Food> ingredients;
     [SerializeField]
-    private int currentItemsCount=0;
+    private int currentItemsCount = 0;
 
     [SerializeField]
     private int totalItemsCount;
     public bool tutorialMode;
+
+
+    [SerializeField]
+    private GameObject knife;
     public void AddItemCut(Food item) {
         if (ingredients.Contains(item)) {
             currentItemsCount++;
@@ -20,6 +26,12 @@ public class CuttingManager : MonoBehaviour
 
         if (currentItemsCount== totalItemsCount && tutorialMode) {
             TutorialManager.Instance.NextText();
+            ActiveKnife();
         }
+    }
+
+    public void ActiveKnife() {
+        knife.GetComponent<BoxCollider>().enabled = true;
+        knife.GetComponent<Rigidbody>().isKinematic = true;
     }
 }
